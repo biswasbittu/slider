33b6654fce790fd1df0a57077e79bdb09afa0c7a
biswasbittu <biswasbittu7415@yahoo.com>
2023-03-17 09:09:16 +0530

    slider done

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,25 @@
-let imgIndex=0;
-setInterval(()=>{
-//   console.log(imgIndex);
-  imgIndex++;
+const images = [
+    './images/pic-1.jpg',
+    './images/pic-2.jpg',
+    './images/pic-3.jpg',
+    './images/pic-4.jpg',
+    './images/pic-5.jpg',
+    './images/pic-6.jpg',
+    './images/pic-7.jpg',
+    './images/pic-8.jpg',
+    './images/pic-9.jpg',
+]
 
-},1000)
+let imgIndex = 0;
+const sliderImg=document.getElementById('slider-img')
+setInterval(() => {
+    if(imgIndex===images.length){
+         imgIndex=0;
+        
+    }
+    const imgUrl=images[imgIndex];
+    console.log(imgIndex,imgUrl);
+
+    sliderImg.setAttribute('src',imgUrl)
+    imgIndex++;
+}, 3000)
